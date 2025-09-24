4b86060acc2d54146b1155ef70221fa7a0176935
Akari <majikainan@gmail.com>
2025-09-24 16:49:19 +0900

    変更1　.

---
+++ example.py
@@ -0,0 +1,24 @@
+#ステップ150　ハッカソン事前テストの課題用.pyファイルです。
+
+#1
+print("Hello,World")
+
+#2
+greet = ("こんにちは")
+print (greet)
+
+#3
+name = input("名前を入力してください")
+print("私の名前は",name,"です。")
+
+#4
+def get_greet():
+    print("おはようございます")
+    
+get_greet()
+
+#5
+b = int(input("1つめの数字を入力"))
+c = int(input("2つ目の数字を入力"))
+add = b + c
+print("1つ目と2つ目の数字の合計は",add)
